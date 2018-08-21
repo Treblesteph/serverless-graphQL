98e95ad389636e2d68dedd2efd6c549900b1ceca
Stephanie Unna <steph.unna@gmail.com>
2018-08-21 16:53:24 +0100

    Creating query for clients to call

---
+++ src/graphql/queries/Post.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import {
+	GraphQLList,
+	GraphQLString,
+	GraphQLNonNull
+} from 'graphql'
+
+import PostType from '../types/Post'
+import PostResolver from '../resolvers/Post'
+ 
+
+module.exports = {
+ 
+	index() {
+		return {
+			type: new GraphQLList(PostType),
+			description: 'This will return all the posts we find in the given subreddit.',
+			args: {
+				subreddit: {
+					type: GraphQLString,
+					description: 'Please enter subreddit name',
+				}
+			},
+			resolve(parent, args, context, info) {
+				return PostResolver.index(args);
+			}
+		}
+	},
+ 
+}
