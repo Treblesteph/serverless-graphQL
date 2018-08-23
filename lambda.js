3a2470d2d551064bfdda4f924796c4ef2fcb8d2e
Stephanie Unna <steph.unna@gmail.com>
2018-08-23 14:22:12 +0100

    Adding lambda function file

---
+++ lambda.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import awsServerlessExpress from 'aws-serverless-express'
+import app from './src/app'
+
+const server = awsServerlessExpress.createServer(app)
+const handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+
+export  default handler
